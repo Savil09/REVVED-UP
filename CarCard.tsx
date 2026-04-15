e39dd0ab3df73bb44ff8d12d24f3c0858cafed39
Savil09 <savilseldonpinto@gmail.com>
2026-04-15 12:44:34 -0400

    Update CarCard.tsx

--- CarCard.tsx
+++ CarCard.tsx
@@ -2,8 +2,8 @@ import { Link } from 'react-router-dom';
 import { motion } from 'motion/react';
 import { Heart, Zap, Gauge, ArrowRight } from 'lucide-react';
 import { Car } from '../types';
-import { cn } from '../lib/utils';
-import { useDreamGarage } from '../hooks/useDreamGarage';
+import { cn } from '../utils';
+import { useDreamGarage } from '../useDreamGarage';
 
 interface CarCardProps {
   car: Car;
